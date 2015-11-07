24d00b3b69523fffec53c6b243ca3fb920429ca4
eznibe <enbertran@gmail.com>
2015-11-07 16:17:06 +0000

    php show only error, for cloud9 useful

--- include/headers.php
+++ include/headers.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ERROR);
 header('Access-Control-Allow-Headers: accept, content-type'); 
 header('Access-Control-Allow-Origin: *'); 
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
