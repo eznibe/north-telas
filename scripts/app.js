cc3195cb1a946c7420074d31241f7e4afb8b74af
Ezequiel Bertran <enbertran@gmail.com>
2016-02-10 19:33:33 -0300

    added config to avoid IE cache on gets

--- scripts/app.js
+++ scripts/app.js
@@ -77,6 +77,19 @@ angular.module("vsko.stock", [
         $httpProvider.interceptors.push('ClearSearchBox');
     }
     ])
+		.config(['$httpProvider', function ($httpProvider) {
+			//initialize get if not there
+			if (!$httpProvider.defaults.headers.get) {
+					$httpProvider.defaults.headers.get = {};
+			}
+
+			//disable IE ajax request caching
+			$httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+			// extra
+			$httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+			$httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+    }
+    ])
     .config(['$routeProvider', '$locationProvider', '$translateProvider', function ($routeProvider, $locationProvider, $translateProvider) {
 
         $routeProvider
