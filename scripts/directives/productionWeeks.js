92d307a5e26460274a25a145bfc2e6be984d19f2
Ezequiel Bertran <enbertran@gmail.com>
2016-08-28 16:01:53 -0300

    set different colors for week rows

--- scripts/directives/productionWeeks.js
+++ scripts/directives/productionWeeks.js
@@ -16,7 +16,7 @@ angular.module('vsko.stock')
                            {number: 2, startDay: scope.nextWeeks[1].start, endDay: scope.nextWeeks[1].end, month: 'Month.'+scope.nextWeeks[1].month, backgroundColor: '#ffff99'},
                            {number: 3, startDay: scope.nextWeeks[2].start, endDay: scope.nextWeeks[2].end, month: 'Month.'+scope.nextWeeks[2].month, backgroundColor: '#a3d3ac'},
                            {number: 4, startDay: scope.nextWeeks[3].start, endDay: scope.nextWeeks[3].end, month: 'Month.'+scope.nextWeeks[3].month, backgroundColor: '#ffdab9'},
-                           {number: 5, startDay: scope.nextWeeks[4].start, endDay: scope.nextWeeks[4].end, month: 'Month.'+scope.nextWeeks[4].month},
+                           {number: 5, startDay: scope.nextWeeks[4].start, endDay: scope.nextWeeks[4].end, month: 'Month.'+scope.nextWeeks[4].month, backgroundColor: '#ffee99'},
                            {number: 6, startDay: scope.nextWeeks[5].start, endDay: scope.nextWeeks[5].end, month: 'Month.'+scope.nextWeeks[5].month},
                            {number: 7, startDay: scope.nextWeeks[6].start, endDay: scope.nextWeeks[6].end, month: 'Month.'+scope.nextWeeks[6].month},
                            {number: 8, label: 'Sri Lanka'},
