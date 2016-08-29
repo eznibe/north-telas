e5fb9e65b826d3cc3758adb1797a75c6e1dc1f5a
Ezequiel Bertran <enbertran@gmail.com>
2016-08-29 17:29:44 -0300

    add user from rootscope in timing log

--- scripts/services/utils.js
+++ scripts/services/utils.js
@@ -1,7 +1,7 @@
 /**
  * Created by matthias.snellings on 10/07/2014.
  */
-angular.module('vsko.stock').factory('Utils',[ '$translate', '$http', function ($translate, $http) { //eslint-disable-line
+angular.module('vsko.stock').factory('Utils',[ '$translate', '$http', '$rootScope', function ($translate, $http, $rootScope) { //eslint-disable-line
   var that = {};
   var baseUrl = telasAPIUrl;
 
@@ -16,6 +16,7 @@ angular.module('vsko.stock').factory('Utils',[ '$translate', '$http', function (
                    url: url,
                    service, service,
                    method: method,
+                   user: $rootScope.user.name,
                    entity: JSON.stringify(entity)};
     $http.post(baseUrl + 'timing_POST.php', payload);
   };
