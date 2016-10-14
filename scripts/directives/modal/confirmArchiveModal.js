b9885566a21013072f5bc3620dd0e3b7b5d1c673
Ezequiel Bertran <enbertran@gmail.com>
2016-10-13 21:26:04 -0300

    change archive dispatch confirm message

--- scripts/directives/modal/confirmArchiveModal.js
+++ scripts/directives/modal/confirmArchiveModal.js
@@ -2,7 +2,7 @@
 
 angular.module('vsko.stock')
 
-.directive('confirmArchiveModal', function($modal, Stock) {
+.directive('confirmArchiveModal', function($modal, $translate, Stock) {
 
     return {
           restrict: 'E',
@@ -14,7 +14,7 @@ angular.module('vsko.stock')
         	  var index = attrs.index;
 
 
-        	  $scope.showArchiveConfirmModal = function(selected, dateFieldName, callbackFn) {
+        	  $scope.showArchiveConfirmModal = function(selected, dateFieldName, callbackFn, messageKey) {
 
         		  $scope.selected = selected;
 
@@ -23,12 +23,18 @@ angular.module('vsko.stock')
 
         		  var d = new Date();
         		  $scope.entity = {date: dateFieldName && selected[dateFieldName]
-                                              ? selected[dateFieldName] 
+                                              ? selected[dateFieldName]
                                               : (d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear())};
 
               $scope.modalArchiveConfirm = $modal({template: 'views/modal/confirmArchive.html', show: false, scope: $scope, callback: callback, backdrop:'static'});
 
               $scope.modalArchiveConfirm.$promise.then($scope.modalArchiveConfirm.show);
+
+              if (messageKey) {
+                $scope.confirmMessage = $translate.instant(messageKey);
+              } else {
+                delete $scope.confirmMessage;
+              }
         	  };
 
               $scope.confirmArchive = function() {
--- scripts/directives/modal/confirmModal.js
+++ scripts/directives/modal/confirmModal.js
@@ -2,53 +2,57 @@
 
 angular.module('vsko.stock')
 
-.directive('confirmModal', function($modal, Stock) {
-        
+.directive('confirmModal', function($modal, $translate, Stock) {
+
     return {
           restrict: 'E',
           link: function postLink(scope, element, attrs) {
-              
+
         	  var $scope = scope;
         	  var callback = attrs.callback;
         	  var params = attrs.params;
         	  var index = attrs.index;
-        	  
-        	  
+
+
         	  $scope.showConfirmDeleteModal = function(selected, callbackFn) {
-        		  
+
         		  $scope.selected = selected;
-        		  
+
         		  if(callbackFn)
         			  callback = callbackFn;
-        		  
-                  $scope.modalConfirm = $modal({template: 'views/modal/confirmDelete.html', show: false, scope: $scope, callback: callback, backdrop:'static'});
 
-                  $scope.modalConfirm.$promise.then($scope.modalConfirm.show);
+              $scope.modalConfirm = $modal({template: 'views/modal/confirmDelete.html', show: false, scope: $scope, callback: callback, backdrop:'static'});
+
+              $scope.modalConfirm.$promise.then($scope.modalConfirm.show);
         	  };
-        	  
-        	  $scope.showConfirmModal = function(selected, callbackFn) {
-        		  
+
+        	  $scope.showConfirmModal = function(selected, callbackFn, messageKey) {
+
         		  $scope.selected = selected;
-        		  
+
         		  if(callbackFn)
         			  callback = callbackFn;
-        		  
-                  $scope.modalConfirm = $modal({template: 'views/modal/confirm.html', show: false, scope: $scope, callback: callback, backdrop:'static'});
 
-                  $scope.modalConfirm.$promise.then($scope.modalConfirm.show);
+              $scope.modalConfirm = $modal({template: 'views/modal/confirm.html', show: false, scope: $scope, callback: callback, backdrop:'static'});
+
+              $scope.modalConfirm.$promise.then($scope.modalConfirm.show);
+
+              if (messageKey) {
+                $scope.confirmMessage = $translate.instant(messageKey);
+              }
         	  };
-        	  
+
               $scope.confirm = function() {
 
             	  var param = index ? $scope[params][$scope[index]] : $scope.selected;
-            	  
+
             	  if(typeof($scope.modalConfirm.$options.callback) == "function") {
             		  $scope.modalConfirm.$options.callback(param);
             	  }
             	  else {
             		  $scope[$scope.modalConfirm.$options.callback](param);
               	  }
-            	  
+
             	  $scope.modalConfirm.hide();
               };
           }
