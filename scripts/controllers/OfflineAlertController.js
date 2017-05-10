a96bf86bdc02212a494ab33164c66744ba8799bf
Ezequiel Bertran <enbertran@gmail.com>
2017-05-09 21:47:15 -0300

    fix in offline message

--- scripts/controllers/OfflineAlertController.js
+++ scripts/controllers/OfflineAlertController.js
@@ -3,8 +3,4 @@
 angular.module('vsko.stock').controller('OfflineAlertCtrl', ['$scope', '$rootScope', '$translate', '$location', 'Utils', 'Users', 'userRoles',
                                                              function ($scope, $rootScope, $translate, $location, Utils, Users, userRoles) {
 
-   $translate('Offline alert').then(function(value) {
-     $scope.offlinemessage = value;
-   });
-
 }]);
