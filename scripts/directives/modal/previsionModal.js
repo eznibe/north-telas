5d1044a889b3262367097aa5e658a13e9e17228b
Ezequiel Bertran <enbertran@gmail.com>
2017-09-11 23:50:43 -0300

    sort used previsions in ac desc

--- scripts/directives/modal/previsionModal.js
+++ scripts/directives/modal/previsionModal.js
@@ -259,6 +259,18 @@ angular.module('vsko.stock')
 
               isInPlotterWithCuts($scope.prevision);
 
+              // order previsions of the autocomplete by sequence number desc when same order number
+              $scope.previsionsForAC = $scope.previsions.slice().sort(function(p1, p2) {
+                var p1Split = p1.orderNumber.split('-');
+                var p2Split = p2.orderNumber.split('-');
+
+                if (p1Split[0] === p2Split[0]) {
+                  return p1Split[1] <= p2Split[1] ? 1 : -1;
+                }
+
+                return p1.orderNumber <= p2.orderNumber ? -1 : 1;
+              });
+
           	  $scope.modalPrevision = $modal({template: 'views/modal/prevision.html', show: false, scope: $scope, backdrop:'static', animation:'am-fade-and-slide-top'});
 
               $scope.modalPrevision.$promise.then($scope.modalPrevision.show);
