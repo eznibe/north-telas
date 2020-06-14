5148852a03b7cf39341d8a83536c9b879f686ce1
Ezequiel Bertran <enbertran@gmail.com>
2020-06-14 18:20:32 -0300

    test config for vence auto deploy

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,6 @@
 
 // PRODUCTION keys
-//var telasAPIUrl = 'http://northserver.no-ip.biz/telas/api/';
+// var telasAPIUrl = 'http://nssudamerica-intranet.com/api/';
 //var remoteAPIUrl = 'http://localhost:90/telas/api/';
 //var googleAnalytics = true;
 
@@ -10,7 +10,8 @@
 // var productionFolders = {OD: '0B3ufmcTU0qqcM0U0WkZwcHlkcEE', CA: '0B3ufmcTU0qqcbjlQcEhXMVZIaHc', NY: '0B3ufmcTU0qqcM0U0WkZwcHlkcEE', RA: '0B3ufmcTU0qqcM0U0WkZwcHlkcEE', DA: '0B3ufmcTU0qqcM0U0WkZwcHlkcEE', REP: '0B3ufmcTU0qqcd054ZkJMVDQ1YzA', LO: '0B3ufmcTU0qqcWmdWdXRJN0RyNDA'};
 
 // LOCAL keys
-var telasAPIUrl = 'http://localhost:90/telas/api/';
+// var telasAPIUrl = 'http://localhost:90/telas/api/';
+var telasAPIUrl = 'http://north-telas.now.sh/api/';
 //var telasAPIUrl = 'http://192.168.0.210:90/telas/api/';
 var remoteAPIUrl = 'http://northserver.byethost7.com/remoteapi/';
 // var remoteAPIUrl = 'http://northserver.no-ip.biz/telas/remoteapi/';
@@ -24,5 +25,15 @@ var productionFolders = {OD: '0B7pV9nMgniCgMFFGLWFuaTZ3bk0', CA: '0B7pV9nMgniCgS
 
 var workticketsFolder = '1bknFUHd6yiYsn0hp1PeakW513BRK315N';
 
+// var workticketsFolder = '1lrbWyaWAdxZxzw3rVLZYSVGIJUU090l_';
+// var workticketsFolder = '1lgCCyHEcZh7vVBNPrvY7VwwoDUQ-lYsM';
+// var workticketsFolder = '1MKxeFtuI2QXGh3S1i7mSHTTf9SKP1rHD';
+
+// var workticketsFolder = '197ybabB0kX15XoBdZ6FO5jfjfqDxpT-E';
+
 var drive_client_id = '331531529676-qgg6ph5m6t6m9mjqkcb027knvh0chu96.apps.googleusercontent.com';
 var drive_api_key = 'AIzaSyCGEMPtsFzhy2PaqoWyKyiwkdux9irxklA';
+
+// different google app 'north sails - worktickets'
+// var drive_client_id = '905864529586-glu800fdjf932uqq6a1v2jn59aufbs5l.apps.googleusercontent.com';
+// var drive_api_key = 'AIzaSyDlXGc6arSqIycaLIjZxlWUd-VTw9eDHas';
